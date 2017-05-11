482fd75891f974400b0178d8e06b90f0945a8838
tomegz <tomaszbubala@o2.pl>
2017-05-11 09:20:54 +0200

    add traverse function

--- binary_search_trie.js
+++ binary_search_trie.js
@@ -23,18 +23,58 @@ BinarySearchTrie.prototype = {
               return;
             } 
             let currentNode = this.root;
-            //probably should use while for this
-            if(number < currentNode.value) {
-                /* go left
-                   check if theres no value already
-                   go further or set value and return */
-            } else if(number > currentNode.value) {
-                /* go right
-                   check if theres no value already
-                   go further or set value and return */
-            }
+            /*look for a place to put a number until there's an empty spot (null)
+              go left if number is lesser than current node's value and go right if it's greater*/
+            while(currentNode) {
+                if(number < currentNode.value) {
+                    if(!currentNode.left) {
+                        currentNode.left = node;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else if(number > currentNode.value) {
+                    if(!currentNode.right) {
+                        currentNode.right = node;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                } else {
+                    //ignore this number because it's already inserted
+                    break;
+                }
+            } 
+            //while end
+            return;
+            
         });
+    },
+    
+    print: function() {
+        console.log(this);
+    },
+    
+    isBalanced: function() {
+        
+    },
+    
+    traverse: function(helpFunc) {
+        
+        function walk(node) {
+            if(node) {
+                helpFunc.call(this, node.value);
+                if(node.left) {
+                    walk(node.left);
+                }
+                if(node.right) {
+                    walk(node.right);
+                }
+            }
+        }
+        
+        walk(this.root); //start at tree root
     }
 };
 myTrie = new BinarySearchTrie();
-myTrie.add([22, 4, 8, 23, 42, 15, 36, 12, 11, 50, 30, 21]);
+//myTrie.add([22, 4, 8, 23, 42, 15, 36, 12, 11, 50, 30, 21, 22]);
+myTrie.add([10, 5, 12, 3, 4,7,15])
+myTrie.print();
